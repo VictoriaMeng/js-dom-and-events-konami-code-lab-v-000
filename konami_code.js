7c3f9f5a08bf6e26a34ada23838e51ba5ea52a90
Victoria Meng <nights1ststar@protonmail.com>
2018-08-20 03:26:29 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0
 
-function init(code) {
+function init(e) {
   // Write your JavaScript code inside the init() function
+  const key = parseInt(e.detail || e.which);
+  if key === code[index] {
+    index++;
 
+    if (index === code.length ) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
 
 const body = document.getElementbyId('body');
