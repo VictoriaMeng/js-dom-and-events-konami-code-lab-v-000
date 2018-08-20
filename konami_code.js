9a614eebfc751fd1477a02105ea827675b4794da
Victoria Meng <nights1ststar@protonmail.com>
2018-08-20 03:39:45 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,23 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let index = 0
 
-function init(e) {
-  // Write your JavaScript code inside the init() function
-  const key = parseInt(e.detail || e.which);
-  if key === code[index] {
-    index++;
+function init() {
+  let index = 0;
 
-    if (index === code.length ) {
-      alert("Hurray!");
+// This is the function that would be invoked by the event listener.
+  document.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
 
+      if (index === code.length) {
+        alert("Hurray!");
+
+        index = 0;
+      }
+    } else {
       index = 0;
-    };
-  } else {
-    index = 0;
-  };
-};
-
-const body = document.getElementbyId('body');
-body.addEventListener('keydown', init(e) {
-  console.log(e.which);
-})
+    }
+  })
+}
